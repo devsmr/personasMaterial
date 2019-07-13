09b62947e12fcef20af0d355938e237874b5e80b
Leandro <leandro9115@gmail.com>
2019-07-13 16:57:17 -0500

    guardar y listar terminados

--- app/src/main/java/com/example/personasmeterial/AgregarPersonas.java
+++ app/src/main/java/com/example/personasmeterial/AgregarPersonas.java
@@ -1,5 +1,6 @@
 package com.example.personasmeterial;
 
+import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,6 +20,7 @@ public class AgregarPersonas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_personas);
 
+        fotos = new ArrayList<>();
         fotos.add(R.drawable.images);
         fotos.add(R.drawable.images2);
         fotos.add(R.drawable.images3);
@@ -38,7 +40,8 @@ public class AgregarPersonas extends AppCompatActivity {
         p.guardar();
         limpiar();
 
-        Snackbar.make(v,"Persona Guardada Exitosamente",Snackbar.LENGTH_SHORT).show();
+        //Snackbar.make(v,"Persona Guardada Exitosamente",Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(v,getString(R.string.mensaje),Snackbar.LENGTH_SHORT).show();
     }
 
     public void limpiar(View v){
@@ -58,5 +61,12 @@ public class AgregarPersonas extends AppCompatActivity {
         Random r = new Random();
         fotoSeleccionada = r.nextInt(this.fotos.size());
         return fotos.get(fotoSeleccionada);
+    }
+
+    public void onBackPressed(){
+        finish();
+        Intent i = new Intent(AgregarPersonas.this,MainActivity.class);
+        startActivity(i);
+
     }
 }
--- app/src/main/java/com/example/personasmeterial/MainActivity.java
+++ app/src/main/java/com/example/personasmeterial/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.personasmeterial;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private RecyclerView lstOpciones;
+    private Intent i;
+    private ArrayList<Persona> personas;
+    private LinearLayoutManager llm;
+    private String db = "Personas";
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,42 +39,47 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //crear las personas
-        //String id, int foto, String nombre, String apellido
-       // Persona p = new Persona("per1",0,"leandro","gut");
-        //p.eliminar();
+        lstOpciones = findViewById(R.id.lstPersonas);
+        personas = new ArrayList<>();
+        final AdaptadorPersona adapter = new AdaptadorPersona(personas);
 
+        llm  = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayout.VERTICAL);
+        lstOpciones.setLayoutManager(llm);
+        lstOpciones.setAdapter(adapter);
 
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child(db).addValueEventListener(new ValueEventListener() {
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+            //Estar pendiente cuando hay un cambio en producción (cuando se agregue algo)
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                personas.clear();
+                if(dataSnapshot.exists()){
+
+                    for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                        Persona p = snapshot.getValue(Persona.class);
+                        personas.add(p);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+                Datos.setPersonas(personas);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void agregarPersona (View v){
+
+        i = new Intent(MainActivity.this,AgregarPersonas.class);
+        startActivity(i);
+        finish();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
+
